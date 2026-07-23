b3bdd7894dea431d67ff518077abb3977a379a68
Candida Cook <c34139087@gmail.com>
2026-07-22 22:17:04 -0500

    first upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Little Z's Lawn Care - offline shell
+const CACHE = 'littlezs-v1';
+const FILES = ['./', './index.html', './card.html', './manifest.json',
+               './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
